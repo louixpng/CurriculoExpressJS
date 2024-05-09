fe07679c1a13c6b9dfe943f52a8df510b51b28fa
louixpng <64700304+louixpng@users.noreply.github.com>
2024-05-09 22:23:46 +0000

    Atualizado Sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -2,15 +2,6 @@ require("dotenv").config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_CONNECTION_STRING,
-    dialect: "postgres",
-    port: process.env.DB_PORT
-  }
-);
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
 
 module.exports = sequelize;
